2bdcff728276b67581db3181d602e16987257041
Aidan Christopher <gandamovies@gmail.com>
2023-09-30 01:14:33 -0700

    added new rundown section 'Days'

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
-const session = require('express-session');
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const session = require('express-session')
 const PrismaClient = require('@prisma/client').PrismaClient
 const prisma = new PrismaClient()
-const prisma_functions = require("./prisma/prisma_controller");
+const prisma_functions = require("./prisma/prisma_controller")
 const { stat } = require('fs');
 const url = "https://wild-rose-antelope-veil.cyclic.cloud/"
 const net = require('net');
 const { PromiseSocket } = require("promise-socket")
+const { parseISO, format, weeksToDays } = require('date-fns')
+const { da } = require('date-fns/locale')
 
 const app = express();
 
@@ -41,17 +43,12 @@ app.get("/", async (req, res) => {
   const promiseSocket = new PromiseSocket(socket)
   let alive = true
   try {
-    console.log(1)
     await promiseSocket.connect(parseInt(process.env.testPORT), String(process.env.testIP))
-    console.log(2)
   }
   catch {
-    console.log(3)
     alive = false
   }
-  console.log(4)
   if (alive) {
-    console.log(5)
     let values
     if (req.query.yes == undefined) {
       values = {
@@ -70,7 +67,6 @@ app.get("/", async (req, res) => {
     res.render("index", { values })
   }
   else {
-    console.log(6)
     res.render("offline")
   }
 })
@@ -121,7 +117,58 @@ app.post("/retrieve", async (req, res) => {
 
 app.get("/rundown", async (req, res) => {
   let recent_cap = 5
-
+  let weekStats = {
+    sunday: {
+      str:"Sunday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    monday: {
+      str:"Monday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    tuesday: {
+      str:"Tuesday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    wednesday: {
+      str:"Wednesday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    thursday: {
+      str:"Thursday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    friday: {
+      str:"Friday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+    saturday: {
+      str:"Saturday",
+      yes: 0,
+      no: 0,
+      su: 0
+    },
+  }
+  let weekFacts = {
+    most_trans:0,
+    most_trans_str:"",
+    best_signup:0,
+    best_signup_str:"",
+    best_usage:0,
+    best_usage_str:"",
+  }
   const data = (await prisma_functions.rundown("2938068")).reverse()
   const stats = {
     trans_count: 0,
@@ -139,6 +186,43 @@ app.get("/rundown", async (req, res) => {
   }
 
   for (item in data) {
+    data[item].datestring = data[item].date.toISOString().split('T')[0]
+    data[item].dateDay = format(parseISO(data[item].datestring), 'EEEE')
+    if (data[item].dateDay === "Sunday") {
+      weekStats.sunday.yes += data[item].yes;
+      weekStats.sunday.no += data[item].no;
+      weekStats.sunday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Monday") {
+      weekStats.monday.yes += data[item].yes;
+      weekStats.monday.no += data[item].no;
+      weekStats.monday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Tuesday") {
+      weekStats.tuesday.yes += data[item].yes;
+      weekStats.tuesday.no += data[item].no;
+      weekStats.tuesday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Wednesday") {
+      weekStats.wednesday.yes += data[item].yes;
+      weekStats.wednesday.no += data[item].no;
+      weekStats.wednesday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Thursday") {
+      weekStats.thursday.yes += data[item].yes;
+      weekStats.thursday.no += data[item].no;
+      weekStats.thursday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Friday") {
+      weekStats.friday.yes += data[item].yes;
+      weekStats.friday.no += data[item].no;
+      weekStats.friday.su += data[item].su;
+    }
+    else if (data[item].dateDay === "Saturday") {
+      weekStats.saturday.yes += data[item].yes;
+      weekStats.saturday.no += data[item].no;
+      weekStats.saturday.su += data[item].su;
+    }
 
     stats.trans_count = stats.trans_count + data[item].total_trans
     stats.total_yes = stats.total_yes + data[item].yes
@@ -161,13 +245,51 @@ app.get("/rundown", async (req, res) => {
 
   }
 
+  for (day in weekStats) {
+    let total = weekStats[day].yes + weekStats[day].no + weekStats[day].su
+    let yes_percentage = (weekStats[day].yes / total) * 100
+    let su_percentage = ((weekStats[day].yes+weekStats[day].su) / total) * 100
+    weekStats[day].signup_per = String(yes_percentage.toFixed(2))+"%"
+    weekStats[day].usage_per = String(su_percentage.toFixed(2))+"%"
+  }
   stats.signup_per = String(((stats.total_yes / stats.trans_count) * 100).toFixed(2)) + "%"
   stats.usage_per = String((((stats.total_yes + stats.total_su) / stats.trans_count) * 100).toFixed(2)) + "%"
 
   stats.recent_signup_per = String(((stats.recent_yes / stats.recent_trans_count) * 100).toFixed(2)) + "%"
   stats.recent_usage_per = String((((stats.recent_yes + stats.recent_su) / stats.recent_trans_count) * 100).toFixed(2)) + "%"
 
-  res.render("rundown", { data, stats, recent_cap })
+  for (day in weekStats) {
+    if ((weekStats[day].yes+weekStats[day].no+weekStats[day].su)>weekFacts.most_trans){
+      weekFacts.most_trans = weekStats[day].yes+weekStats[day].no+weekStats[day].su
+      weekFacts.most_trans_str = weekStats[day].str
+    }
+    if (parseFloat(weekStats[day].signup_per.replace('%',''))>weekFacts.best_signup){
+      weekFacts.best_signup = parseFloat(weekStats[day].signup_per.replace('%',''))
+      weekFacts.best_signup_str = weekStats[day].str
+    }
+    if (parseFloat(weekStats[day].usage_per.replace('%',''))>weekFacts.best_usage){
+      weekFacts.best_usage = parseFloat(weekStats[day].usage_per.replace('%',''))
+      weekFacts.best_usage_str = weekStats[day].str
+    }
+  }
+
+  function calculateTotal(obj) {
+    return obj.yes + obj.no + obj.su;
+  }
+
+  const byTotal = { ...weekStats };
+  const bySignupPer = { ...weekStats };
+  const byUsagePer = { ...weekStats };
+
+  const sortedByTotal = Object.entries(byTotal).sort((a, b) => calculateTotal(b[1]) - calculateTotal(a[1]));
+  const sortedBySignupPer = Object.entries(bySignupPer).sort((a, b) => parseFloat(b[1].signup_per) - parseFloat(a[1].signup_per));
+  const sortedByUsagePer = Object.entries(byUsagePer).sort((a, b) => parseFloat(b[1].usage_per) - parseFloat(a[1].usage_per));
+  
+  const weekStats_trans = Object.fromEntries(sortedByTotal);
+  const weekStats_signup = Object.fromEntries(sortedBySignupPer);
+  const weekStats_usage = Object.fromEntries(sortedByUsagePer);
+
+  res.render("rundown", { data, stats, recent_cap, weekStats, weekStats_trans, weekStats_signup, weekStats_usage, weekFacts })
 })
 
 const PORT = process.env.PORT || 8000;
